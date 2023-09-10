71ff83c63a1796a065777938051d2206f5bdcae1
Kevin <kevinu774@gmail.com>
2023-09-10 12:50:49 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 
 // mongoose.connect("mongodb://127.0.0.1:27017/Trip-Planner")
 
-mongoose.connect("mongodb+srv://explorenomadictrips:SRKS2003@cluster0.lu4bqm2.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://explorenomadictrips:SRKS2003@cluster0.lu4bqm2.mongodb.net/Trip-Planner?retryWrites=true&w=majority")
 
 
 
